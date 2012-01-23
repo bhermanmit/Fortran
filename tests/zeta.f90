06804f8df18f49ff696e56b32c1c6e64bb51cfd9
Bryan Herman <bherman@mit.edu>
2012-01-23 12:44:36 -0800

    initial zeta program commit

---
+++ tests/zeta.f90
@@ -0,0 +1,19 @@
+program zeta
+
+implicit none
+
+end program zeta
+
+subroutine zeta_sub()
+
+implicit none
+
+end subroutine zeta_sub
+
+function zeta_fun()
+
+implicit none
+
+  real :: zeta_fun
+
+end function zeta_fun 
